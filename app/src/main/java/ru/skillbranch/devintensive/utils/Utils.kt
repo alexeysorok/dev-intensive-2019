8066dc68f59f641de828d76553845e99b914b733
alexe <alexe@W10SSD>
2019-06-29 14:15:12 +0300

    task: Utils.transliteration

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -43,4 +43,72 @@ object Utils {
 
         return result
     }
+
+    fun transliteration(payload: String, divider: String = " "): String {
+
+        // словарь
+        val mapCirilic: Map<String, String>  = mapOf("а" to "a",
+            "б" to "b",
+            "в" to "v",
+            "г" to "g",
+            "д" to "d",
+            "е" to "e",
+            "ё" to "e",
+            "ж" to "zh",
+            "з" to "z",
+            "и" to "i",
+            "й" to "i",
+            "к" to "k",
+            "л" to "l",
+            "м" to "m",
+            "н" to "n",
+            "о" to "o",
+            "п" to "p",
+            "р" to "r",
+            "с" to "s",
+            "т" to "t",
+            "у" to "u",
+            "ф" to "f",
+            "х" to "h",
+            "ц" to "c",
+            "ч" to "ch",
+            "ш" to "sh",
+            "щ" to "sh'",
+            "ъ" to "",
+            "ы" to "i",
+            "ь" to "",
+            "э" to "e",
+            "ю" to "yu",
+            "я" to "ya")
+
+        // вспомогательный массив
+        var listResultWord: MutableList<String> = mutableListOf()
+
+        var listWords = payload.toList()
+        for (word in listWords) {
+
+            var isUpper = word.isUpperCase()
+            var item = word.toLowerCase().toString()
+
+            if (mapCirilic.containsKey(item)) {
+
+                // добавляем в список если буква есть в словаре
+                if (isUpper) {listResultWord.add(mapCirilic.get(item).toString().toUpperCase())}
+
+                else {listResultWord.add(mapCirilic.get(item).toString())}
+
+
+            }
+            // если нет в списке доабавляем без измененний
+            else listResultWord.add(word.toString())
+        }
+
+        val resultWord= listResultWord.joinToString("")
+        val listResult = resultWord.split( " ")
+        val result = listResult.joinToString(divider)
+
+
+        return result
+
+    }
 }
